2caaa266687c4b4db1e4c831ea5a1a489b124761
Danylo Honcharov <danilkogonchar@gmail.com>
2023-09-30 13:21:24 +0300

    Created Node entity

--- Core/Entities/Note.cs
+++ Core/Entities/Note.cs
@@ -1,6 +1,19 @@
-﻿namespace Core.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Entities;
 
 public class Note
 {
+    [Key]
+    public int Id { get; set; }
     
+    [Required]
+    [StringLength(50)]
+    public string? Title { get; set; }
+    
+    [Required]
+    public DateTime CreationTime { get; set; }
+    
+    [Required]
+    public string? Description { get; set; }
 }
