196a597ed50e45f7092e13c0bfc62dba27589eae
Danylo Honcharov <danilkogonchar@gmail.com>
2023-09-30 12:46:25 +0300

    Set up start architecture and database connection

---
+++ Infrastructure/Data/DataBaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class DataBaseContext : DbContext
+{
+    public DbSet<Note> Notes { get; set; }
+    
+    public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
+}
--- Web/Program.cs
+++ Web/Program.cs
@@ -1,5 +1,7 @@
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
 using Web.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +11,9 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 
+//Add database context
+builder.Services.AddDbContext<DataBaseContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
